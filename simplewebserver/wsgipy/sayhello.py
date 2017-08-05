74f5a807016b575a4f2d6ad893a6fdae4b7a328d
tianpo <mailtp@foxmail.com>
2017-08-05 19:00:19 +0800

    删除旧版本

--- simplewebserver/wsgipy/sayhello.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-
-def application(env,start_response):
-    status = '200 OK'
-    headers =[
-        ('Content-Type','text/plain')
-    ]
-    start_response(status,headers)
-    return "hello world"
